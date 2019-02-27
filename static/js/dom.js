c6c70a7843d5c5d2eb9753a4042a7dc47c789137
AdamSzucs1 <adaam.szucs@gmail.com>
2019-02-27 12:39:02 +0100

    Frontend part of log in and register modals implemented.

--- static/js/dom.js
+++ static/js/dom.js
@@ -55,3 +55,37 @@ export let dom = {
     },
     // here comes more features
 };
+
+
+    let registerModal = document.getElementById('registermodal');
+    let loginModal = document.getElementById('loginmodal');
+    // Get the button that opens the modal
+    let registerButton = document.getElementById("register");
+    let loginButton = document.getElementById("login");
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    registerButton.onclick = function() {
+      registerModal.style.display = "block";
+    };
+
+    loginButton.onclick = function() {
+      loginModal.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      registerModal.style.display = "none";
+      loginModal.style.display = "none";
+    };
+
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == registerModal || event.target == loginModal) {
+        registerModal.style.display = "none";
+        loginModal.style.display = "none";
+      }
+    };
